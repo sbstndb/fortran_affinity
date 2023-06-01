96fe820e1628cad896d56794a2322732ce0f7083
Dubois Sebastien <sebastien.dubois@onera.fr>
2023-06-01 22:19:19 +0200

    Add cpu affinity
    	-	we can add a spu affinity mask with sched_setaffinity
    	-	C - fortran interface needed

--- c_fortran/c.c
+++ c_fortran/c.c
@@ -1,5 +1,10 @@
-#include <inttypes.h>
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <stdint.h>
+#include <inttypes.h>
+
 
 
 
@@ -9,3 +14,22 @@ void c_function(int64_t* array, int64_t n) {
     printf("%d\n", array[i]);
   }
 }
+
+
+void set_affinity(int64_t* id_cpu , int64_t len) {
+
+        unsigned long uns_id ;
+
+        cpu_set_t my_set ;
+        CPU_ZERO(&my_set) ;
+        for (int64_t i = 0 ; i < len ; i++){
+                uns_id = (unsigned long) id_cpu[i] ;
+                CPU_SET(uns_id , &my_set);
+        }
+        sched_setaffinity(0, sizeof(cpu_set_t) , &my_set);
+}
+
+
+
+
+
--- c_fortran/f.f90
+++ c_fortran/f.f90
@@ -1,25 +1,37 @@
 ! file: main.f90
 
 program main
-  use iso_c_binding
-  implicit none
-  integer(C_INT64_T), allocatable, target :: array(:)
-  integer(C_INT64_T) :: n, i
-  interface
-    subroutine c_function(array, n) bind(C, name="c_function")
-      use iso_c_binding
-      integer(C_INT64_T), intent(inout) :: array(*)
-      integer(C_INT64_T), value, intent(in) :: n
-    end subroutine c_function
-  end interface
+        use iso_c_binding
+        implicit none
+        integer(C_INT64_T), allocatable, target :: array(:)
+        integer(C_INT64_T) :: n, i
+        interface
+                subroutine c_function(array, n) bind(C, name="c_function")
+                        use iso_c_binding
+                        integer(C_INT64_T), intent(inout) :: array(*)
+                        integer(C_INT64_T), value, intent(in) :: n
+                end subroutine c_function
+        end interface
 
-  n = 10
-  allocate(array(n))
+        interface
+        subroutine set_affinity(array, n) bind(C, name="set_affinity")
+                use iso_c_binding
+                        integer(C_INT64_T), intent(inout) :: array(*)
+                        integer(C_INT64_T), value, intent(in) :: n
+                end subroutine set_affinity
+        end interface
 
-  do i = 1, n
-    array(i) = i
-  end do
 
-  call c_function(array, n)
+
+        n = 10
+        allocate(array(n))
+
+        do i = 1, n
+                array(i) = i
+        end do
+
+        call c_function(array, n)
+
+        call set_affinity(array, n)
 
 end program main
